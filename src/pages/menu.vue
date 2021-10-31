f8eceac2344ee41aa3a91aec2a15f2777d0b2e66
Ridwan Maulana <rdwanf49@gmail.com>
2021-10-31 19:10:25 +0700

    chore: add field

--- src/pages/menu.vue
+++ src/pages/menu.vue
@@ -2,6 +2,18 @@
   <v-row justify="center" align="center" class="mx-2">
     <v-col cols="12" sm="8" md="6">
       <h3 class="mb-8 text-center">Cari Kost Impianmu</h3>
+      <div class="d-flex justify-space-between">
+            <p class="mt-10">Panjang</p>
+            <v-col class="d-flex" cols="4" sm="3">
+              <v-text-field label="Panjang(m)" />
+            </v-col>
+      </div>
+      <div class="d-flex justify-space-between">
+          <p class="mt-10">Lebar</p>
+          <v-col class="d-flex" cols="4" sm="3">
+            <v-text-field label="Lebar(m)" />
+          </v-col>
+      </div>
       <div
         v-for="item in items"
         :key="item.property"
