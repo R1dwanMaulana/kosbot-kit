c177bb250aa917a813302ba745fb6317967d355b
Ridwan Maulana <rdwanf49@gmail.com>
2021-11-07 07:21:39 +0700

    feat: add about page

---
+++ src/pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card elevation="4" rounded="lg">
+        <div class="text-center mx-3 py-10">
+          <img src="/about.jpg" alt="logo kosbot" height="200" />
+          <h2>
+            Apikasi prediksi harga kost, praktis dari web browser + support
+            install (app native)
+          </h2>
+          <p class="mt-8">Metode :</p>
+          <li>Multiple Linear Regression</li>
+          <v-alert class="mt-5" border="top" type="warning" outlined>
+            Apabila Anda ingin menyampaikan saran, kritik, dan menemukan
+            kesalahan pada aplikasi. bisa dijelaskan di halaman Github issue
+          </v-alert>
+          <h5 class="mt-15 font-weight-regular">
+            Contents licensed under CC BY-NC-SA 4.0. <br />Made using Nuxt.js
+            and Vuetify. Hosted on Vercel. <br />
+            MIT License © 2021 - present | Ridwan Maulana.
+          </h5>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
