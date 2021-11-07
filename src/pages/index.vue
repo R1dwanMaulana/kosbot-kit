29665cec4dc02c6d8b99721929da87e83430be81
Ridwan Maulana <rdwanf49@gmail.com>
2021-11-07 07:21:04 +0700

    feat: fix landing page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,36 +1,27 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-carousel
-        :show-arrows="false"
-        hide-delimiter-background
-        height="200"
-        cycle
-      >
-        <v-carousel-item
-          v-for="(item, i) in items"
-          :key="i"
-          :src="item.src"
-        ></v-carousel-item>
-      </v-carousel>
+      <img src="/header.jpg" alt="" width="350" />
       <br />
-      <h3>Kosbot</h3>
-      <p class="text-justify">
-        Kosbot adalah aplikasi berbasis web dengan teknologi AI untuk
-        memprediksi harga kost wilayah Malang Raya. Aplikasi ini dapat membantu
-        Anda untuk mencari kost yang tepat.
-      </p>
-      <br />
-      <nuxt-link to="/menu" class="text-decoration-none">
-        <v-btn
-          depressed
-          color="secondary"
-          class="white--text rounded-lg"
-          elevation="10"
-        >
-          Prediksi <v-icon>mdi-arrow-right</v-icon>
-        </v-btn>
-      </nuxt-link>
+      <div class="mx-2">
+        <h2 class="mt-10">Kosbot</h2>
+        <p class="text-justify">
+          Kosbot adalah aplikasi berbasis web dengan teknologi AI untuk
+          memprediksi harga kost di kota Malang. Aplikasi ini dapat membantu
+          Anda untuk mencari kost yang tepat.
+        </p>
+        <br />
+        <nuxt-link to="/menu" class="text-decoration-none">
+          <v-btn
+            depressed
+            color="deep-orange accent-2"
+            class="white--text rounded-lg pr-2 pl-2"
+            elevation="15"
+          >
+            Coba sekarang <v-icon>mdi-arrow-right</v-icon>
+          </v-btn>
+        </nuxt-link>
+      </div>
     </v-col>
   </v-row>
 </template>
@@ -41,16 +32,16 @@ export default {
     return {
       items: [
         {
-          src: '/Slider_Kosbot_1.png'
+          src: "/Slider_Kosbot_1.png",
         },
         {
-          src: '/Slider_Kosbot_2.png'
+          src: "/Slider_Kosbot_2.png",
         },
         {
-          src: '/Slider_Kosbot_3.png'
-        }
-      ]
-    }
-  }
-}
+          src: "/Slider_Kosbot_3.png",
+        },
+      ],
+    };
+  },
+};
 </script>
