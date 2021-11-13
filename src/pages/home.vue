40654a7cd1c72f359c46976e3d7a424f9fd3f9d6
Ridwan Maulana <rdwanf49@gmail.com>
2021-11-13 20:16:30 +0700

    feat: add page home

---
+++ src/pages/home.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <img src="/header.jpg" alt="" width="350" />
+      <br />
+      <div class="mx-2">
+        <h2 class="mt-10">Kosbot</h2>
+        <p class="text-justify">
+          Kosbot adalah aplikasi berbasis web app dengan teknologi
+          <strong>AI</strong> untuk memprediksi harga kost di kota Malang.
+          Aplikasi ini dapat membantu Anda untuk mencari kost yang tepat.
+        </p>
+        <br />
+        <nuxt-link to="/menu" class="text-decoration-none">
+          <v-btn
+            depressed
+            color="pink lighten-1"
+            class="
+              white--text
+              rounded-lg
+              pr-1
+              pl-2
+              text-subtitle-2 text-caption
+              font-weight-medium
+            "
+            elevation="10"
+          >
+            Coba sekarang
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </nuxt-link>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: "/Slider_Kosbot_1.png",
+        },
+        {
+          src: "/Slider_Kosbot_2.png",
+        },
+        {
+          src: "/Slider_Kosbot_3.png",
+        },
+      ],
+    };
+  },
+};
+</script>
